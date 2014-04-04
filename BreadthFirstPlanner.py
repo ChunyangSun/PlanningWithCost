fcb290c1684e27bf2390193c44c653ace9efe7a7
Jane <jane@ubuntu.ubuntu-domain>
2014-04-04 02:52:19 -0400

    draft code, testing needed

--- BreadthFirstPlanner.py
+++ BreadthFirstPlanner.py
@@ -1,3 +1,5 @@
+import numpy
+from SimpleEnvironment import SimpleEnvironment
 
 class BreadthFirstPlanner(object):
     
@@ -6,15 +8,45 @@ class BreadthFirstPlanner(object):
         self.visualize = visualize
         
     def Plan(self, start_config, goal_config):
-        
-        plan = []
-
         # TODO: Here you will implement the breadth first planner
         #  The return path should be a numpy array
         #  of dimension k x n where k is the number of waypoints
         #  and n is the dimension of the robots configuration space
         
-        plan.append(start_config)
+        plan = []
+        recordMarked = []
+        self.tree = RRTTree(planning_env, start_config)
+        sid = 0
+        
+        plan.append(start_config)   
+        queue = [start_config]
+        while len(queue) != 0:
+            # pop the first element in queue FIFO
+            parent = queue.pop(0)
+            # see if that's the goal
+            if parent == goal_config:
+                break
+            else:                 
+                 Children = self.s.GetSuccessors(parent) 
+                 for child in children:
+                    # avoid revisit and check collision
+                    if child not in recordMarked and (not self.s.Collides(child)):
+                        recordMarked.append(child)
+                        queue.append(child)
+                        sid = self.recordMarked.index(parent)
+                        eid = self.tree.AddVertex(child)
+                        self.tree.AddEdge(self, sid, eid)
+                        #visualize the updated tree
+                        self.planning_env.PlotEdge(self, parent, child)
+                   
+
+        print "Tree size: " + str(len(self.tree.vertices))
+        plan = []
+        currVertex = eid
+        while (currVertex != tree.GetRootId()):
+            currVertex = tree.edges[currVertex] 
+            plan.insert(0, tree.vertices[currVertex]) 
         plan.append(goal_config)
-   
+
         return plan
+
--- DepthFirstPlanner.py
+++ DepthFirstPlanner.py
@@ -1,3 +1,5 @@
+from RRTTree import RRTTree
+
 class DepthFirstPlanner(object):
     
     def __init__(self, planning_env, visualize):
@@ -15,6 +17,39 @@ class DepthFirstPlanner(object):
         #  and n is the dimension of the robots configuration space
 
         plan.append(start_config)
-        plan.append(goal_config)
+        if dfs_rec(start_config) == None: return [] # TODO: or return None
+        else: 
+            print "Tree size: " + str(len(self.tree.vertices))
+          
+            currVertex = eid
+            while (currVertex != tree.GetRootId()):
+                currVertex = tree.edges[currVertex] 
+                plan.insert(0, tree.vertices[currVertex]) 
+            plan.append(goal_config)
 
-        return plan
+            return plan
+
+    def dfs_rec(parent):
+        # base case, no children 
+        if(self.planning_env.GetSuccessors(parent) == None):
+            return None
+        elif(parent == goal_config):
+            return 
+        else:
+            Children = self.planning_env.GetSuccessors(parent) 
+            for child in children:
+                # avoid revisit and check collision
+                if child not in recordMarked and (not self.s.Collides(child)):
+                    recordMarked.append(child)
+                    dfs_rec(child)
+                    sid = self.recordMarked.index(parent)
+                    eid = self.tree.AddVertex(child)
+                    self.tree.AddEdge(self, sid, eid)
+                    # visualize the updated tree
+                    self.planning_env.PlotEdge(self, parent, child)
+
+
+
+                   
+
+
